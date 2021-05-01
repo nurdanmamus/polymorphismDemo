ea674e5eb087bdea9401328db5e96fcd1d21b68a
nurdan <nurdan@DESKTOP-A3RNKRN>
2021-05-01 20:05:32 +0300

    Inheritance ~ Polymorphism

---
+++ src/polymorphismDemo/DatabaseLogger.java
@@ -0,0 +1,9 @@
+package polymorphismDemo;
+
+public class DatabaseLogger extends BaseLogger{
+    
+	@Override
+	public void log(String message) {
+		System.out.println("Logged to database");
+	}
+}
